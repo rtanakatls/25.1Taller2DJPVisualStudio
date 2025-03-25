da6b35d5e7bde01d41cd1dda14b92f9464bc14a1
Ricardo Tanaka Terukina <rtanaka@talento.tls.edu.pe>
2025-03-24 19:06:45 -0500

    Created respository

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2DJP251
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            bool continueFlag=true;
+            Console.WriteLine("Introduce tu nombre:");
+            name = Console.ReadLine();
+            Console.WriteLine($"Hola {name}.");
+
+            while (continueFlag)
+            {
+                Console.WriteLine("Introduce tu edad");
+                age = int.Parse(Console.ReadLine());
+                if (age >= 18)
+                {
+                    Console.WriteLine("Ya eres mayor de edad");
+                    continueFlag = false;
+                }
+                else if (age >= 0)
+                {
+                    Console.WriteLine("Eres menor de edad");
+                    continueFlag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Tu edad está mal");
+                }
+            }
+
+
+            
+        }
+    }
+}
