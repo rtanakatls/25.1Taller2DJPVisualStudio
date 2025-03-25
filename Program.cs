0d392c066ef11b93218fbec5644802c9ecfa861f
Ricardo Tanaka Terukina <rtanaka@talento.tls.edu.pe>
2025-03-24 21:11:01 -0500

    Added examples

--- Program.cs
+++ Program.cs
@@ -10,35 +10,116 @@ namespace Taller2DJP251
     {
         static void Main(string[] args)
         {
-            string name;
-            int age;
-            bool continueFlag=true;
-            Console.WriteLine("Introduce tu nombre:");
-            name = Console.ReadLine();
-            Console.WriteLine($"Hola {name}.");
+            /*
+            float a;
+            float b;
 
-            while (continueFlag)
+            Console.WriteLine("Introduce el primer número:");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo número:");
+            b = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"La suma es: {a + b}");
+            Console.WriteLine($"La resta es: {a - b}");
+            Console.WriteLine($"La multiplicación es: {a * b}");
+            if (b == 0)
             {
-                Console.WriteLine("Introduce tu edad");
-                age = int.Parse(Console.ReadLine());
-                if (age >= 18)
+                Console.WriteLine("No se puede dividir entre 0");
+            }
+            else
+            {
+                Console.WriteLine($"La división es: {a / b}");
+                Console.WriteLine($"El módulo es: {a % b}");
+            }
+            */
+
+            /*
+            float days;
+
+            Console.WriteLine("Introducir la cantidad de días");
+            days = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"En {days} días hay {days / 365} años");
+            Console.WriteLine($"En {days} días hay {days / 7} semanas");
+            */
+
+            /*
+            int year;
+
+            Console.WriteLine("Introducir el año:");
+            year=int.Parse(Console.ReadLine());
+
+            if (year % 4 == 0)
+            {
+                if(year % 100 == 0)
                 {
-                    Console.WriteLine("Ya eres mayor de edad");
-                    continueFlag = false;
-                }
-                else if (age >= 0)
-                {
-                    Console.WriteLine("Eres menor de edad");
-                    continueFlag = false;
+                    if(year % 400 == 0 )
+                    {
+                        Console.WriteLine($"El año {year} es bisiesto");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"El año {year} no es bisiesto");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Tu edad está mal");
+                    Console.WriteLine($"El año {year} es bisiesto");
                 }
+            }
+            else
+            {
+                Console.WriteLine($"El año {year} no es bisiesto");
+            }*/
+            /*
+            int a;
+
+            Console.WriteLine("Introduce en número: ");
+            a=int.Parse(Console.ReadLine());
+
+            if (a > 0)
+            {
+                Console.WriteLine($"{a} es positivo");
+            }
+            else if (a < 0)
+            {
+                Console.WriteLine($"{a} es negativo");
+            }
+            else
+            {
+                Console.WriteLine($"{a} es cero");
+            }
+            */
+
+            int a;
+
+            Console.WriteLine("Introduce el número");
+            a=int.Parse(Console.ReadLine());
+            if (a % 7 == 0 && a % 13 == 0)
+            {
+                Console.WriteLine($"{a} es divisible entre 7 y 13");
+            }
+            else if (a % 7 == 0)
+            {
+                Console.WriteLine($"{a} es divisible entre 7");
+            }
+            else if (a % 13 == 0)
+            {
+                Console.WriteLine($"{a} es divisible entre 13");
+            }
+            else
+            {
+                Console.WriteLine($"{a} no es divisible ni entre 7 ni entre 13");
             }
 
 
-            
+
+
+
+
         }
+
+
+        
     }
 }
